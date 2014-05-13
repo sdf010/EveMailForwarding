88ee46a3d88e291b77f09c6c38e717b3f48f91c5
Francis Screene <chillauinho@me.com>
2014-05-13 15:55:51 +0100

    Preliminarily done, need to further change storage of password etc.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 EveMailForwarding
 =================
+
+A small python script that automatically forwards eve mails to your real email account.
+
+To set this up:
+
+1. Get the script along with the sampleconfig.cfg (you should rename this to "config.cfg")
+
+2. Edit the config and fill in your information
+	
+	I currently don't have access to an smtp so I made a new googlemail account to use. If you happen
+	to have access to one, just edit the script (line 62) accordingly. Fill in the config file with your
+	information, including a limited api key (Only needs MailMessage and MailBodies, predefined link: 
+	https://community.eveonline.com/support/api-key/CreatePredefined?accessMask=2560 ), as well as your
+	characters ID (get this by running an ID request for your name, spaces in your name equal %20 in the request
+	example: If the characters name is "Martin Sonderberg", the request would be 
+	https://api.eveonline.com/eve/CharacterID.xml.aspx?names=Martin%20Sonderberg).
+	
+3. Profit
+
+	Simply run the script in order to get emailes about changes, set it up on a virtual server
+	
+Word of Caution: Textfiles are not the safest form of storing unencrypted passwords so use this method
+at your own risk.
--- forward.py
+++ forward.py
@@ -2,6 +2,9 @@ __author__ = 'Francis'
 
 import smtplib
 import urllib
+import re
+import sys
+import fileinput
 import xml.etree.ElementTree as ET
 from datetime import datetime
 
@@ -10,7 +13,8 @@ info = dict()
 with open("config.cfg") as f:
     lines = f.readlines()
 for line in lines:
-    tList = line.split("=")
+    pLine = line.replace("\n", "")
+    tList = pLine.split("=")
     info[tList[0]] = tList[1]
 
 
@@ -19,26 +23,52 @@ mailMessageQuery = "https://api.eveonline.com/char/MailMessages.xml.aspx?keyID=%
 messageTree = ET.parse(urllib.urlopen(mailMessageQuery)).getroot()
 headerList = []
 headers = messageTree.find('result/rowset')
-
 emailsAfter = datetime.strptime(info['emailsAfter'], '%Y-%m-%d %H:%M:%S')
+mailCounter = 0
+#get the mail message headers from the eve api
 for child in headers:
     dict = child.attrib
     sentDate = datetime.strptime(dict['sentDate'], '%Y-%m-%d %H:%M:%S')
-    if sentDate > emailsAfter:
+    if sentDate > emailsAfter and dict['senderID'] != info['characterID']:
+            headerList.append([dict['messageID'], dict['senderName'], dict['title'], dict['sentDate'], ""])
+            mailCounter += 1
 
-    headerList.append(child.attrib)
+#if we havent retrieved any messages we know to be new, terminate execution
+if mailCounter == 0:
+    sys.exit("No mails remain to be fetched")
 
-
+#get the body for every mail and accumulate them in a list
 mailBodyQuery = "https://api.eveonline.com/char/MailBodies.xml.aspx?keyID=%s&vCode=%s&characterID=%s&ids=" % (info['keyID'], info['vCode'], info['characterID'])
-
-for id in ids:
-    mailBodyQuery += id + ","
+for tuple in headerList:
+    mailBodyQuery += tuple[0] + ","
 mailBodyQuery = mailBodyQuery[:-1]
+mailTree = ET.parse(urllib.urlopen(mailBodyQuery)).getroot()
+bodies = mailTree.find('result/rowset')
 
+for child in bodies:
+    for x in range(0,len(headerList)):
+        if headerList[x][0] == child.attrib['messageID']:
+            headerList[x][4] = child.text
+            break
 
-message = ""
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.starttls()
-server.login(info['mailLogin'], info['mailPasswd'])
-server.sendmail(info['mailFrom'], info['mailTo'], message)
+#compile the final message
+message = "\n Mail messages received since: %s \n \n" % (info['emailsAfter'])
+t1MSGLength = len(message)
+for ls in headerList:
+    message += "From: " + ls[1] + "\n Subject:" + ls[2] + "\n Sent: " + ls[3] + "\n \n" + (re.sub(r'</?\w+\s+[^>]*>','',ls[4])).replace("<br>", "") + "\n ======================== \n \n"   #regex needs fixing for double >>
 
+#set up the connection with the google smtp
+if len(message) > t1MSGLength:
+   server = smtplib.SMTP('smtp.gmail.com:587')
+   server.starttls()
+   server.login(info['mailLogin'], info['mailPasswd'])
+   server.sendmail(info['mailFrom'], info['mailTo'], message)
+
+   newest = emailsAfter
+   for ls in headerList:
+       sentDate = datetime.strptime(ls[3], '%Y-%m-%d %H:%M:%S')
+       if sentDate > newest:
+           newest = sentDate
+
+   for line in fileinput.input("config.cfg", inplace=True):
+       print line.replace(("emailsAfter=" + info['emailsAfter']),("emailsAfter=" + newest.strftime('%Y-%m-%d %H:%M:%S'))),
